2a72fba9481c436aacf2b8c601fc35acf5cef16f
Prakash Gupta <prakash.25gupta@gmail.com>
2023-09-25 21:56:13 +0530

    added registerUser route.

--- auth-service-backend/routes/userRoute.js
+++ auth-service-backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
+import { registerUser } from "../controllers/userController.js";
 
 const router = express.Router();
-router.post("/register", ()=>{});
-router.post("/login", ()=>{});
+router.post("/register", );
+router.post("/login", ()=>registerUser);
 router.get("/:userId", ()=>{});
 router.put("/:userId", ()=>{});
 router.get("/logout", ()=>{});
