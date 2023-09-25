9e93c936fbc93c6e984bf7fe077220d22c7a3e46
Prakash Gupta <prakash.25gupta@gmail.com>
2023-09-25 23:00:34 +0530

    Fixed login route

--- auth-service-backend/routes/userRoute.js
+++ auth-service-backend/routes/userRoute.js
@@ -3,7 +3,7 @@ import { loginUser, registerUser } from "../controllers/userController.js";
 
 const router = express.Router();
 router.post("/register", registerUser);
-router.post("/login", () => loginUser);
+router.post("/login", loginUser);
 router.get("/:userId", () => { });
 router.put("/:userId", () => { });
 router.get("/logout", () => { });
