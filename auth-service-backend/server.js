171a6778b44a7d967193a316d7676aba419c1a9b
Prakash Gupta <prakash.25gupta@gmail.com>
2023-09-25 16:20:22 +0530

    Added code for DB connect

---
+++ auth-service-backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.listen(PORT, ()=>{
+    console.log(`Server started on port ${PORT}.`);
+});
