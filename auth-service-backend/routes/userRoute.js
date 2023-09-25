760a762645bcd1c032f7b2a591c44c9294fff41c
Prakash Gupta <prakash.25gupta@gmail.com>
2023-09-25 22:01:17 +0530

    fixed register route.

--- auth-service-backend/routes/userRoute.js
+++ auth-service-backend/routes/userRoute.js
@@ -2,10 +2,10 @@ import express from "express";
 import { registerUser } from "../controllers/userController.js";
 
 const router = express.Router();
-router.post("/register", );
-router.post("/login", ()=>registerUser);
-router.get("/:userId", ()=>{});
-router.put("/:userId", ()=>{});
-router.get("/logout", ()=>{});
+router.post("/register", registerUser);
+router.post("/login", () => { });
+router.get("/:userId", () => { });
+router.put("/:userId", () => { });
+router.get("/logout", () => { });
 
 export default router;
