25ec4a65a48452e0f1341b19894557a005e1c5e1
Prakash Gupta <prakash.25gupta@gmail.com>
2023-09-25 21:04:48 +0530

    Added userRoute.

--- auth-service-backend/server.js
+++ auth-service-backend/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db";
+import userRoute from "./routes/userRoute.js"
 
 dotenv.config();
 connectDB();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+app.use("/api/user", userRoute);
 
 app.listen(PORT, ()=>{
     console.log(`Server started on port ${PORT}.`);
