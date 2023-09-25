5d3189fbdb0047524ec0afcc261fed75482101c6
Prakash Gupta <prakash.25gupta@gmail.com>
2023-09-25 22:59:38 +0530

    Added loginUser controller function in route

--- auth-service-backend/routes/userRoute.js
+++ auth-service-backend/routes/userRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import { loginUser, registerUser } from "../controllers/userController.js";
 
 const router = express.Router();
 router.post("/register", registerUser);
-router.post("/login", () => { });
+router.post("/login", () => loginUser);
 router.get("/:userId", () => { });
 router.put("/:userId", () => { });
 router.get("/logout", () => { });
