e82caf9d1a1dca43df06f1b83544f8fca45552a8
Prakash Gupta <prakash.25gupta@gmail.com>
2023-09-27 21:39:37 +0530

    defined middleware to protect routes.

---
+++ auth-service-backend/middlewares/authMiddleware.js
@@ -0,0 +1,26 @@
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+
+const protect = asyncHandler(async (req, res) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.userId).select('-password');
+            next();
+        }
+        catch (error) {
+            res.status(401);
+            throw new Error("Not Authorized! Invalid token.");
+        }
+    }
+    else {
+        res.status(401);
+        throw new Error("Not Authorized! Token is not available.");
+    }
+});
+
+
+export { protect }
