bfd3835009d5d208d52441f9b26b744ab8d57de0
Prakash Gupta <prakash.25gupta@gmail.com>
2023-09-28 21:14:31 +0530

    Fixed protect() & added console logs

--- auth-service-backend/middlewares/authMiddleware.js
+++ auth-service-backend/middlewares/authMiddleware.js
@@ -2,23 +2,23 @@ import asyncHandler from "express-async-handler";
 import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
-const protect = asyncHandler(async (req, res) => {
+const protect = asyncHandler(async (req, res, next) => {
     const token = req.cookies.jwt;
-    console.log("token : ", token);
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.userId).select('-password');
-            console.log("decoded : ", decoded);
-            console.log("user : ", req.user);
-            return next();
+            console.log("authMiddleware.js > protect() > req.user :", req.user);
+            next();
         }
         catch (error) {
+            console.log("authMiddleware.js > protect() > token :", token);
             res.status(401);
             throw new Error("Not Authorized! Invalid token.");
         }
     }
     else {
+        console.log("authMiddleware.js > protect() > token :", token);
         res.status(401);
         throw new Error("Not Authorized! Token is not available.");
     }
