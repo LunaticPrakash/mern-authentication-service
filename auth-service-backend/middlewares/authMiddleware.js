f6f9a900a05435c23d109c865c02b0be0075fbf9
Prakash Gupta <prakash.25gupta@gmail.com>
2023-09-28 20:43:27 +0530

    added console logs

--- auth-service-backend/middlewares/authMiddleware.js
+++ auth-service-backend/middlewares/authMiddleware.js
@@ -3,13 +3,15 @@ import jwt from "jsonwebtoken";
 import User from "../models/userModel.js";
 
 const protect = asyncHandler(async (req, res) => {
-    const token = req.cookies ? req.cookies.jwt : null;
-
+    const token = req.cookies.jwt;
+    console.log("token : ", token);
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
             req.user = await User.findById(decoded.userId).select('-password');
-            next();
+            console.log("decoded : ", decoded);
+            console.log("user : ", req.user);
+            return next();
         }
         catch (error) {
             res.status(401);
